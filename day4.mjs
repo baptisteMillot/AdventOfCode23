1a522446da3daec67d9d12aa41eed9cff29219f7
Baptiste Millot <baptiste.millot@boiron.fr>
2023-12-04 22:11:54 +0100

    add day 4 part two

--- day4.mjs
+++ day4.mjs
@@ -3,15 +3,19 @@ import getInputs from "./utils/utils.mjs";
 class Card {
     id;
     winningNumbers;
-    playedNumber;
+    totalPoint;
 
     constructor(line) {
         const [cardName, cardGame] = line.split(":");
         const [winningNumbers, playedNumber] = cardGame.split("|");
 
-        this.id = cardName.match(/\d/g)[0];
-        this.winningNumbers = winningNumbers.trim().split(" ").filter((number) => number);
-        this.playedNumber = playedNumber.trim().split(" ").filter((number) => number)
+        const id = Array.from(cardName.matchAll(/\d/g), x => x[0]).join("");
+        this.id = parseInt(id);
+        const winningNumbersClean = winningNumbers.trim().split(" ").filter((number) => number);
+        const playedNumberClean = playedNumber.trim().split(" ").filter((number) => number)
+
+        this.winningNumbers = playedNumberClean.filter((number) => winningNumbersClean.includes(number));
+        this.totalPoint = this.winningNumbers.reduce((total) => total === 0 ? 1 : total * 2, 0);
     }
 }
 
@@ -19,13 +23,25 @@ function day4PartOne() {
     const inputLines = getInputs("./inputs/day4.txt");
 
     const cards = inputLines.map((line) => new Card(line));
-    const result = cards.reduce((totalPoint, card) => {
-        const playedWinningNumbers = card.playedNumber.filter((number) => card.winningNumbers.includes(number));
+    const result = cards.reduce((totalPoint, card) => totalPoint + card.totalPoint, 0);
 
-        return totalPoint + playedWinningNumbers.reduce((total) => total === 0 ? 1 : total * 2, 0);
-    }, 0);
     console.log(result)
-    
 };
 
-day4PartOne();
+function day4PartTwo() {
+    const inputLines = getInputs("./inputs/day4.txt");
+
+    const cards = inputLines.map((line) => new Card(line));
+
+    for (let i = 0; i < cards.length; i++) {
+        const card = cards[i];
+
+        for (let j = card.id + 1; j <= card.id + card.winningNumbers.length; j++) {
+            cards.push(cards.find((c) => c.id === j));
+        }
+    }
+
+    console.log(cards.length)
+};
+
+day4PartTwo();
